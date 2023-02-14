d4780b1651a1a01df6590e3d9ce214ab9dec39bb
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2023-02-14 09:02:37 -0500

    create new service

--- controllers/serviceController.js
+++ controllers/serviceController.js
@@ -1,16 +1,19 @@
-const {body, validationResult} = require("express-validator");
-const Service = require('../models/service')
+const { body, validationResult } = require('express-validator');
+const mongoose = require('mongoose');
+const Service = require('../models/service');
+
 exports.createService = [
-    body('name').trim().escape().notEmpty().withMessage('Input must not be empty'),
-
-
-
-    (req, res) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) return res.status(400).json(errors);
-    res.json('Hello miter Mihir')
-    new Service({
-
-    });
-    }
+    body('name').trim().escape().notEmpty().withMessage('Name must not be empty'),
+    body('description').trim().escape().notEmpty().withMessage('Description must not be empty'),
+    async (req, res) => {
+        const errors = validationResult(req);
+        if (!errors.isEmpty()) {
+            return res.status(400).json(errors);
+        }
+        const service = new Service({
+            name: req.body['name'],
+            description: req.body['description'],
+        });
+        return res.json(await service.save());
+    },
 ];
