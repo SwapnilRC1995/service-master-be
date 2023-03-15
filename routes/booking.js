a2357b4f605d998ab53098f72a14c8d558e7354a
Mihir Deshpande <mihir.v.deshpande@outlook.com>
2023-03-15 16:30:53 -0400

    add routes for booking

--- routes/booking.js
+++ routes/booking.js
@@ -2,6 +2,16 @@ const express = require('express');
 const router = express.Router();
 const bookingController = require('../controllers/bookingController');
 
-router.get('/', bookingController.getBookings);
+router.get('/', checkIfAdminOrCurrenUserSignedIn, bookingController.getBookings);
+
+router.get('/:_id', checkIfAdminOrCurrenUserSignedIn, bookingController.getBooking);
+
+router.get('/customer/:_id', checkIfAdminOrCurrenUserSignedIn, bookingController.getBookingByCustomerId);
+
+router.put('/:_id', checkIfAdminSignedIn, bookinController.updateService);
+
+router.delete('/:_id', checkIfAdminSignedIn, bookingController.deleteBooking);
+
+router.post('/', checkIfAdminSignedIn, bookingController.createBooking);
 
 module.exports = router;
